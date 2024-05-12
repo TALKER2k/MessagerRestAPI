e08d54d67cb10d873edab68448a970349e93ddd8
user <nunikasnikun26504228@yandex.ru>
2024-05-12 23:32:19 +0300

    исправлена ошибка записи ролей пользователю

--- src/main/resources/db/changelog/1.0/202412052238_003_init_some_users.sql
+++ src/main/resources/db/changelog/1.0/202412052238_003_init_some_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO users VALUES
     (1,'user1', '', '', 'user1@mail.com', '$2a$10$WeVR/Cd68PiLvodrehbqgO67mNKS7JquORFUTT/1cKcGXYQgUnHCK', 'Active');
 
-INSERT INTO roles VALUES (1, 'ADMIN'),
-                         (2, 'USER');
+INSERT INTO roles VALUES (1, 'ROLE_ADMIN'),
+                         (2, 'ROLE_USER');
 
 INSERT INTO user_roles VALUES (1, 1);
 INSERT INTO user_roles VALUES (1, 2);
